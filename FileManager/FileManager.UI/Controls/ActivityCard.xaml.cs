02eb55f5f2db444f827e5715f75781b0916e398f
Nitheesh Govind <nitheesh.govind@gmail.com>
2020-12-23 22:02:30 +0530

    Activity Card added

---
+++ FileManager/FileManager.UI/Controls/ActivityCard.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FileManager.UI.Controls
+{
+    /// <summary>
+    /// Interaction logic for ActivityCard.xaml
+    /// </summary>
+    public partial class ActivityCard : UserControl
+    {
+        public ActivityCard()
+        {
+            InitializeComponent();
+        }
+    }
+}
